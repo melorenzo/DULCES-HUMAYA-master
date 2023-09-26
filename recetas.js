8bac8cc62f771e728382bd2f7fc0a999cc9e1367
Matias Lorenzo <melorenzo@ymail.com>
2023-09-26 16:24:51 -0300

    .

--- recetas.js
+++ recetas.js
@@ -1,7 +1,5 @@
 // Función para obtener recetas de la API
 const obtenerRecetas= async()=>{
-    // Aquí puedes hacer una solicitud a la API de recetas
-    // Puedes usar fetch u otra librería para hacer la solicitud
     const url = 'https://the-birthday-cake-db.p.rapidapi.com/';
     const options = {
 	method: 'GET',
@@ -10,11 +8,9 @@ const obtenerRecetas= async()=>{
 		'X-RapidAPI-Host': 'the-birthday-cake-db.p.rapidapi.com'
 	}
 };
-    // Ejemplo de solicitud usando fetch
     fetch(url, options)
         .then(response => response.json())
         .then(data => {
-            // Luego de obtener los datos, puedes crear las tarjetas de recetas
             crearTarjetasRecetas(data);
         })
         .catch(error => {
@@ -28,46 +24,33 @@ const obtenerRecetas= async()=>{
             console.error(error);
         }
 }
-
-
-
-
 // Función para crear tarjetas de recetas
 function crearTarjetasRecetas(recetas) {
     const recetasContainer = document.getElementById('recetas-container');
     const limite = 3;
-
-    // Recorre las recetas y crea una tarjeta para cada una
     recetas.slice(0, limite).forEach(receta => {
         const tarjeta = document.createElement('div');
         tarjeta.classList.add('receta-card');
 
-        // Agregar titulo a la receta
         const titulo = document.createElement('h4');
         titulo.textContent = receta.title;
         tarjeta.appendChild(titulo);
 
-        // Agrega la imagen de la receta
         const imagen = document.createElement('img');
         imagen.src = receta.image;
         tarjeta.appendChild(imagen);
 
-        // Agrega la descripción de la receta
         const descripcion = document.createElement('p');
         descripcion.textContent = receta.difficulty;
         tarjeta.appendChild(descripcion);
 
-        // Agrega el botón con el título de la sección Recetas
         const boton = document.createElement('button');
         boton.textContent = 'Ver Receta';
         boton.addEventListener('click', () => {
-            // Aquí puedes definir la lógica para mostrar la receta completa
         });
         tarjeta.appendChild(boton);
 
         recetasContainer.appendChild(tarjeta);
     });
 }
-
-// Llama a la función para obtener las recetas al cargar la página
 obtenerRecetas();
